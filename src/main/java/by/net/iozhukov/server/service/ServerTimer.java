e7212e47228162dafd681a642242e0f9fa0c8099
IlyaBeetle <ilya.beetle@gmail.com>
2018-06-05 22:08:28 +0300

    Upgrade method close() in the class Server. Add doc comments.

--- src/main/java/by/net/iozhukov/server/service/ServerTimer.java
+++ src/main/java/by/net/iozhukov/server/service/ServerTimer.java
@@ -1,4 +1,4 @@
-package by.net.iozhukov.utilits;
+package by.net.iozhukov.server.service;
 
 import java.util.Timer;
 import java.util.TimerTask;
@@ -6,15 +6,15 @@ import java.util.TimerTask;
 import org.apache.log4j.Level;
 import org.apache.log4j.Logger;
 
-import by.net.iozhukov.server.service.Server;
-
 /**
+ * A class extends class Thread. Needed for creating an off-timer instance for
+ * the server passed in the constructor. <br>
+ * Runs on the object of this class using the {@link ServerTimer # start ()}
+ * method
  * 
- * 
- * 
- * @author Ilya Zhukov (ilya.beetle@gmail.com)
+ * @author Ilya Zhukov
  */
-public class ServerTimer implements Runnable {
+public class ServerTimer extends Thread {
 
 	private static final Long SECONDS_PER_MINUTE = 60000L;
 
@@ -22,9 +22,12 @@ public class ServerTimer implements Runnable {
 	private Long time;
 
 	/**
+	 * The constructor will prepare the settings for the timer.
 	 * 
 	 * @param server
+	 *            - Server which must be stopped after a specified time
 	 * @param time
+	 *            - Time through which the specified server will be stopped.
 	 */
 	public ServerTimer(Server server, Long time) {
 		this.server = server;
@@ -44,6 +47,6 @@ public class ServerTimer implements Runnable {
 				server.stop();
 			}
 
-		}, time - 5000L);// Minus timeout when finalize
+		}, time);
 	}
 }
